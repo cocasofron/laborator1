bfb294f7cd8ce49766765ebbff589001232be2b7
corinasofron1 <corinasofron1@gmail.com>
2019-05-05 12:38:03 +0300

    project for lab 1

---
+++ WebApplication2/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebApplication2.Models
+{
+    public class Movie
+    {
+        //[Key()]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Runtime { get; set; }
+        public double Rating { get; set; }
+        public string Storyline { get; set; }
+    }
+}
---
+++ WebApplication2/Models/MoviesDbSeeder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class MoviesDbSeeder
+    {
+        public static void Initialize(MoviesDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any movies.
+            if (context.Movies.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Movies.AddRange(
+                new Movie
+                {
+                    Title = "SomeMovie",
+                    Runtime = 1230,
+                    Rating = 9,
+                    Storyline = "the storyline of some movie"
+                },
+                new Movie
+                {
+                    Title = "AnotherMovie",
+                    Runtime = 1300,
+                    Rating = 8,
+                    Storyline = "the storyline of another movie"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
